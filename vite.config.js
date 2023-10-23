ae877f8c2fa391cadbfc9f471aa43668685b6166
VanHuy79 <ngo.huy.van.2001@gmail.com>
2023-10-23 10:31:54 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-
-module.exports = {
-    publicPath: 'see-weather'
-  }
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: "/see-weather"
+})
